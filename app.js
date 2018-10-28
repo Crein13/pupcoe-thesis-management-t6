56c18e8a87eaf79691fbf48e9b51d52c3f59486f
Crein13 <benz.matias13@gmail.com>
2018-10-28 17:38:26 +0800

    update

--- app.js
+++ app.js
@@ -342,10 +342,10 @@ app.get('/faculty/class', function (req, res) {
 
 app.get('/faculty/class/:id', function (req, res) {
   faculty.classList({id: req.user.id}, function (studentList) {
+      student_id: req.user.student_id,
+      class_id: req.user.class_id
     faculty.noClassList({}, function  (noClassList) {
       res.render('faculty/class_detail', {
-        student_id: req.user.student_id,
-        class_id:req.user.class_id,
         student_number: req.user.student_number,
         first_name: req.user.first_name,
         last_name: req.user.last_name,
